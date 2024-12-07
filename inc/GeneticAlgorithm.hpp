2fcb50033e46c9e1056fc0a0f57e4a6375fa1f4e
leggen-assis <souzaferreira437@gmail.com>
2024-12-07 16:45:37 -0300

    virtual functions now are public

--- inc/GeneticAlgorithm.hpp
+++ inc/GeneticAlgorithm.hpp
@@ -16,30 +16,32 @@ class GeneticAlgorithm {
 		std::vector<Chromosome> population;
 		std::vector<std::string> sub_groups;
 	    Chromosome best_chromosome;
-    
-		virtual Chromosome generateChromosome(size_t);
-        virtual void createPopulation(size_t , size_t);
-        std::vector<std::string> binaryToDecimal(const std::vector<std::string>&);
-        virtual void fitness(Chromosome&);
 
         Chromosome& chooseBestSolution(Chromosome&, Chromosome&);
         Chromosome& chooseWorstSolution(Chromosome&, Chromosome&);
 
         Chromosome& tournamentSelection(const std::vector<Chromosome>&);
-
-       	virtual Chromosome crossOver(const Chromosome&, const Chromosome&);
-        virtual void mutation(Chromosome&);
+       	
        	std::vector<Chromosome>& elitism(float mutation_rate);
         std::vector<Chromosome>& createNewPopulation();
 
 	public:
-
+	
 		GeneticAlgorithm(size_t population_size, size_t genes_size,
 			       	float mutation_rate, float elitism_rate):
         				population_size(population_size), genes_size(genes_size),
 				       	mutation_rate(mutation_rate), elitism_rate(elitism_rate),
                         population(population_size), sub_groups(0) {}
-	
+                        
+		virtual Chromosome generateChromosome(size_t);
+        virtual void createPopulation(size_t , size_t);
+        std::vector<std::string> binaryToDecimal(const std::vector<std::string>&);
+        virtual void fitness(Chromosome&);
+        
+        virtual Chromosome crossOver(const Chromosome&, const Chromosome&);
+        virtual void mutation(Chromosome&);
+			
+			
 		Chromosome run(size_t);	
         std::string binaryToDecimal(std::string);
         std::vector<std::string> readValues(const Chromosome&, size_t);
